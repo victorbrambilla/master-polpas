1a3657e77362110a4dda7dbb7a5c307f5058a782
victorbrambilla <88890771+victorbrambilla@users.noreply.github.com>
2021-09-10 19:51:42 -0400

    confirmar

--- script.js
+++ script.js
@@ -64,3 +64,5 @@ if(window.matchMedia("(max-width: 480px)").matches){
     document.querySelector('#mobileimg2').setAttribute('src', 'assets/images/bgmobile2.jpg')
     document.querySelector('#mobileimg3').setAttribute('src', 'assets/images/bgmobile2.jpg')
 }
+
+

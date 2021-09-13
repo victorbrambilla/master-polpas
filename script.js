fe25f4aebec93cb4c53335001104cde02ec435e9
victorbrambilla <88890771+victorbrambilla@users.noreply.github.com>
2021-09-13 15:44:09 -0400

    confirmar

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function scroll1(){
     }
 
     if (window.matchMedia("(max-width: 375px)").matches) {
-        window.scrollTo(0, 2472);
+        window.scrollTo(0, 2415);
     }
         
     
@@ -38,8 +38,23 @@ function scroll2(){
     }
 
     if (window.matchMedia("(max-width: 375px)").matches) {
-        window.scrollTo(0, 2825);
+        window.scrollTo(0, 2997);
     }
+}
+
+function scroll4(){
+    if (window.matchMedia("(max-width: 1920px)").matches) {
+        window.scrollTo(0, 4703);
+    }
+
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+        window.scrollTo(0, 3691);
+    }
+
+    if (window.matchMedia("(max-width: 375px)").matches) {
+        window.scrollTo(0, 3954);
+    }
+    
 }
 
 
@@ -49,29 +64,15 @@ function scroll3(){
     }
 
     if (window.matchMedia("(max-width: 1024px)").matches) {
-        window.scrollTo(0, 2928);
+        window.scrollTo(0, 4225);
     }
 
     if (window.matchMedia("(max-width: 375px)").matches) {
-        window.scrollTo(0, 3754);
+        window.scrollTo(0, 4754);
     }
     
 }
 
-function scroll4(){
-    if (window.matchMedia("(max-width: 1920px)").matches) {
-        window.scrollTo(0, 4703);
-    }
-
-    if (window.matchMedia("(max-width: 1024px)").matches) {
-        window.scrollTo(0, 2928);
-    }
-
-    if (window.matchMedia("(max-width: 375px)").matches) {
-        window.scrollTo(0, 3754);
-    }
-    
-}
 
 
 
@@ -79,6 +80,11 @@ if(window.matchMedia("(max-width: 480px)").matches){
     document.querySelector('#mobileimg1').setAttribute('src', 'assets/images/bgmobile.jpg')
     document.querySelector('#mobileimg2').setAttribute('src', 'assets/images/bgmobile2.jpg')
     document.querySelector('#mobileimg3').setAttribute('src', 'assets/images/polpa.jpg')
+
+    document.querySelector('#product-mobile1').setAttribute('src', 'assets/images/produto1mobile.png')
+    document.querySelector('#product-mobile2').setAttribute('src', 'assets/images/produto2mobile.png')
+    document.querySelector('#product-mobile3').setAttribute('src', 'assets/images/produto3mobile.png')
+    
 }
 
 
